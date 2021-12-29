a3030f0011adbae4ff823026025dcee08975507d
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2021-12-29 23:17:28 +0200

    tables.model

--- tables/controller.py
+++ tables/controller.py
@@ -1,8 +1,8 @@
-from PySide6.QtWidgets import QTreeView
-
-from google.cloud import bigquery
+from PySide6.QtWidgets import QTreeView, QAbstractItemView
+from PySide6.QtCore import QItemSelectionModel
 
 import configuration
+import tables.model as model
 
 
 class Controller:
@@ -10,7 +10,29 @@ class Controller:
         self.view = view
         self.config = config
 
-        if self.config.project is not None:
-            client = bigquery.Client(project=self.config.project)
-            for i in client.list_datasets():
-                print(i.dataset_id)
+        self.view.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.view.setUniformRowHeights(True)
+
+        self.model = model.Model(config=self.config)
+        self.model.setHorizontalHeaderLabels(['col1', 'col2', 'col3'])
+        self.view.setModel(self.model)
+
+        for i in range(3):
+            parent1 = model.Item('Family {}. Some long status text for sp'.format(i))
+            for j in range(3):
+                child1 = model.Item('Child {}'.format(i*3+j))
+                child2 = model.Item('row: {}, col: {}'.format(i, j+1))
+                child3 = model.Item('row: {}, col: {}'.format(i, j+2))
+                parent1.appendRow([child1, child2, child3])
+            self.model.appendRow(parent1)
+            # span container columns
+            self.view.setFirstColumnSpanned(i, view.rootIndex(), True)
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # expand third container
+        index = self.model.indexFromItem(parent1)
+        self.view.expand(index)
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # select last row
+        selmod = self.view.selectionModel()
+        index2 = self.model.indexFromItem(child3)
+        selmod.select(index2, QItemSelectionModel.Select|QItemSelectionModel.Rows)
